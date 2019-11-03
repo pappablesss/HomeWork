443f8b22e51597feef1456f67468700a252e4f63
pappablesss <nikiokguy@gmail.com>
2019-11-03 17:25:18 +0200

    commit -m

--- task/src/App.js
+++ task/src/App.js
@@ -1,29 +1,41 @@
 import React, { Component } from 'react';
 import StyledButton from './StyledButton';
 
-class App extends Component {
-  constructor(props){
-   super(props);
-   this.state = {
-     step: { answer: '' },
-   };
-   this.markAs = this.markAs.bind(this);
+const texts = [
+    ['Text 1'],
+    ['Text 2'],
+    ['Text 3'],
+    ['Text 4'],
+  ];
+  
+  class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        clickedText: []
+      };
+    }
+    
+    handleClick = (i) => {
+          this.setState({ clickedText: texts[i] });
+    };
+  
+    render() {
+      const { clickedText } = this.state;
+      return (
+        <div>
+          {texts.map((text, i) => 
+            <StyledButton key={i} onClick={() => this.handleClick(i)}>Click me {i + 1}</StyledButton>
+          )}
+          {clickedText.length > 0 && <p>I clicked on button with text:</p>}
+          <ul>
+            {clickedText.map((t, i) => (
+              <li key={`text-${i}`}>{t}</li>
+            ))}
+         </ul>
+        </div>
+      );
+    }
   }
-
-  markAs() {
-   this.setState({ step: { answer: 'done' } });
-  }
-
-  render(){
-   return (
-     <StyledButton
-       variant="raised"
-       color={(this.state.step.answer === 'done' ? 'primary' : 'default')}
-       onClick={this.markAs}
-      > Done
-      </StyledButton>
-   );
-  }
-}
-
+  
 export default App
